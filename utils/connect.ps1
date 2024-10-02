722f563d1754f416c3cbf4ae59890a5b6f81b7eb
Danut Florian <dan@attacksimulator.com>
2024-10-03 02:02:53 +0300

    Added module installation check

--- utils/connect.ps1
+++ utils/connect.ps1
@@ -1,5 +1,10 @@
 function connect {
-    Install-Module -Name ExchangeOnlineManagement
+    # check if the module is already installed
+    if (-not (Get-Module -Name ExchangeOnlineManagement -ListAvailable)) {
+        Write-Host "The module ExchangeOnlineManagement is not installed. Installing it now." -ForegroundColor yellow
+        # install the module
+        Install-Module -Name ExchangeOnlineManagement -Force -AllowClobber
+    } 
 
     Import-Module ExchangeOnlineManagement
 
