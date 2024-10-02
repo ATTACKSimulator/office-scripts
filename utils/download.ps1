9d03f3d12e05c73c50514bc68fc17ec7fdfa87da
Danut Florian <dan@attacksimulator.com>
2024-10-03 02:01:11 +0300

    Added more information download for ats servers

---
+++ utils/download.ps1
@@ -0,0 +1,26 @@
+function downloadData {
+    param(
+        [string]$url
+    )
+    
+    $response = Invoke-WebRequest -Uri $url
+    $data = $response.Content -split "`n"
+
+    return $data
+}
+
+function downloadEmailDomains {
+    Write-Host "Downloading the list of domains from ATTACK Simulator..."
+    return downloadData "https://app.attacksimulator.com/whitelist/email-domains"
+}
+
+function downloadIps {
+    Write-Host "Downloading the list of IP addresses from ATTACK Simulator..."
+    return downloadData "https://app.attacksimulator.com/whitelist/ips"
+}
+
+function downloadLandingDomains {
+    Write-Host "Downloading the list of landing domains from ATTACK Simulator..."
+    return downloadData "https://app.attacksimulator.com/whitelist/landing-domains"
+}
+
